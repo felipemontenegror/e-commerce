7c47dc62eaaafbdfd0378640c0c1a883ec72272b
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-05-13 20:27:03 -0300

    listagem js

--- pizza.js
+++ pizza.js
@@ -1,8 +1,55 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'./img/piza1.jpg', price:20.19, sizes: ['100g', '500g', '800g'] },
-    {id:2, name:'Calabresa', img:'./img/piza1.jpg', price:21.19, sizes: ['100g', '500g', '800g'] },
-    {id:3, name:'4 queijos', img:'./img/piza1.jpg', price:22.19, sizes: ['100g', '500g', '800g'] },
-    {id:4, name:'chocolate', img:'./img/piza1.jpg', price:23.19, sizes: ['100g', '500g', '800g'] },
-    {id:5, name:'frango', img:'./img/piza1.jpg', price:24.19, sizes: ['100g', '500g', '800g'] },
-    {id:6, name:'peperoni', img:'./img/piza1.jpg', price:25.19, sizes: ['100g', '500g', '800g'] },
+    {
+    id:1, 
+    name:'Mussarela', 
+    img:'./img/piza1.jpg', 
+    price:20.19, 
+    sizes: ['100g', '500g', '800g'], 
+    description: 'pizza só de queijo mussarela' 
+    },
+
+    {
+    id:2, 
+    name:'Calabresa', 
+    img:'./img/piza1.jpg', 
+    price:21, 
+    sizes: ['100g', '500g', '800g'], 
+    description: 'pizza de queijo e calabresa'
+    },
+
+    {
+    id:3, 
+    name:'4 queijos', 
+    img:'./img/piza1.jpg', 
+    price:22.19, 
+    sizes: ['100g', '500g', '800g'], 
+    description: 'pizza de 4 queijos' 
+    },
+
+    {
+    id:4, 
+    name:'chocolate', 
+    img:'./img/piza1.jpg', 
+    price:23.19, 
+    sizes: ['100g', '500g', '800g'], 
+    description: 'pizza de chocolate ao leite' 
+    },
+
+    {
+    id:5, 
+    name:'frango', 
+    img:'./img/piza1.jpg', 
+    price:24.19, 
+    sizes: ['100g', '500g', '800g'], 
+    description: 'pizza só de frango' 
+    },
+
+    {
+    id:6, 
+    name:'peperoni', 
+    img:'./img/piza1.jpg', 
+    price:25.19, 
+    sizes: ['100g', '500g', '800g'], 
+    description: 'pizza de queijo mussarela e peperoni' 
+    },
 ];
--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const c =(el) => document.querySelector(el)
 const cs = (el) => document.querySelectorAll(el)
 
 pizzaJson.map((item, index) => {
- let pizzaItem = c('.models .pizza-item').cloneNode(true) //c=document.querySelector
+    let pizzaItem = c('.models .pizza-item').cloneNode(true) //c=document.querySelector
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+   
 
     c('.pizza-area').append(pizzaItem)
 })
