0570c0a8ef99550e45bb83532e64341ce6989768
Felipe Montenegro Ribeiro <felipe.mribeiro@al.infnet.edu.br>
2021-05-13 21:11:56 -0300

    modal

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
-   
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault()
+
+        c('.PizzaWindowArea').style.opacity = 0
+        c('.PizzaWindowArea').style.display = 'flex'  //altera o estado do display de none para flex
+        setTimeout (() => {
+            c('.PizzaWindowArea').style.opacity = 1
+    }, 2000)
+    })
 
     c('.pizza-area').append(pizzaItem)
 })
